b524b71685fa103f0b1e43c8d7c7ba8e3341b616
Ben Leadbetter <leadbetter.ben@outlook.com>
2023-08-15 17:03:19 +0100

    Add Spotify API with user Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
+import { Scopes, SpotifyApi } from '@spotify/web-api-ts-sdk';
 import styles from './page.module.css'
 
-export default function Home() {
+export default async function Home() {
+  console.log("Searching Spotify for The Beatles...");
+
+  const api = SpotifyApi.withUserAuthorization(
+    process.env.SPOTIFY_CLIENT_ID || "",
+    process.env.REDIRECT_TARGET || "",
+    Scopes.userDetails
+  )
+
+  //   const items = await api.search("The Beatles", ["artist"]);
+
+  //   console.table(items.artists.items.map((item) => ({
+  //       name: item.name,
+  //       followers: item.followers.total,
+  //       popularity: item.popularity,
+  //   })));
+  // })();
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
