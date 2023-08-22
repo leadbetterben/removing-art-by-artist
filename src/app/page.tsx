034e9926c24ab542367b118cd72b3f1292a26439
Ben Leadbetter <leadbetter.ben@outlook.com>
2023-08-22 15:34:52 +0100

    Remove hook to simplify API creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,35 @@
 'use client';
 
-import { AuthorizationCodeWithPKCEStrategy, Scopes, SdkOptions, SearchResults, SpotifyApi } from '@spotify/web-api-ts-sdk';
-import { useEffect, useRef, useState } from 'react';
+import { Scopes, SearchResults, SpotifyApi } from '@spotify/web-api-ts-sdk';
+import { useEffect, useState } from 'react';
 
-export default async function Home() {
-  const sdk = useSpotify( // Call useSpotify function to create SDK
+function Home() {
+  console.log(process.env.SPOTIFY_CLIENT_ID); // Logs correct client ID
+  const spotifyApi = SpotifyApi.withUserAuthorization(
     process.env.SPOTIFY_CLIENT_ID || "",
     process.env.SPOTIFY_REDIRECT_URI || "",
     Scopes.userDetails
   );
-
+  
   // If SDK exists display SpotifySearch otherwise empty
-  return sdk ? (<SpotifySearch sdk={sdk} />) : (<></>);
-}
-
-export function useSpotify(clientId: string, redirectUrl: string, scopes: string[], config?: SdkOptions) {
-
-  const [sdk, setSdk] = useState<SpotifyApi | null>(null);
-  const { current: activeScopes } = useRef(scopes);
-
-  console.log("useSpotify", [clientId, redirectUrl, config, activeScopes]); // This line is logged
-
-  useEffect(() => {
-    console.log("useEffect", [clientId, redirectUrl, config, activeScopes]); // This line is not logged
-    (async () => {
-      const auth = new AuthorizationCodeWithPKCEStrategy(clientId, redirectUrl, activeScopes);
-      const internalSdk = new SpotifyApi(auth, config);
-
-      try {
-        const { authenticated } = await internalSdk.authenticate();
-
-        if (authenticated) {
-          setSdk(() => internalSdk);
-        }
-      } catch (e: Error | unknown) {
-
-        const error = e as Error;
-        if (error && error.message && error.message.includes("No verifier found in cache")) {
-          console.error("If you are seeing this error in a React Development Environment it's because React calls useEffect twice. Using the Spotify SDK performs a token exchange that is only valid once, so React re-rendering this component will result in a second, failed authentication. This will not impact your production applications (or anything running outside of Strict Mode - which is designed for debugging components).", error);
-        } else {
-          console.error(e);
-        }
-      }
-    })();
-  }, [clientId, redirectUrl, config, activeScopes]);
-
-  return sdk;
+  return spotifyApi ? (<SpotifySearch sdk={spotifyApi} />) : (<></>);
 }
 
 function SpotifySearch({ sdk }: { sdk: SpotifyApi}) {
   const [results, setResults] = useState<SearchResults>({} as SearchResults);
 
+  console.log("Begin search"); // Logs
+
   useEffect(() => {
+    console.log("In useEffect"); // Does not log
     (async () => {
+      console.log("In async"); // Does not log
       const results = await sdk.search("The Beatles", ["artist"]);
       setResults(() => results);      
     })();
   }, [sdk]);
+
+  console.log("Finish search"); // Logs
 
   // Generate a table for the results
   const tableRows = results.artists?.items.map((artist) => {
@@ -87,3 +60,5 @@ function SpotifySearch({ sdk }: { sdk: SpotifyApi}) {
     </>
   )
 }
+
+export default Home;
