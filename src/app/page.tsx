ab04b4f08716390a3fd9f9b91c4ba7d64f902d0f
Ben Leadbetter <leadbetter.ben@outlook.com>
2023-08-22 15:19:39 +0100

    Attempt to reproduce Spotify library React example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,89 @@
-import { Scopes, SpotifyApi } from '@spotify/web-api-ts-sdk';
-import styles from './page.module.css'
+'use client';
+
+import { AuthorizationCodeWithPKCEStrategy, Scopes, SdkOptions, SearchResults, SpotifyApi } from '@spotify/web-api-ts-sdk';
+import { useEffect, useRef, useState } from 'react';
 
 export default async function Home() {
-  console.log("Searching Spotify for The Beatles...");
-
-  const api = SpotifyApi.withUserAuthorization(
+  const sdk = useSpotify( // Call useSpotify function to create SDK
     process.env.SPOTIFY_CLIENT_ID || "",
-    process.env.REDIRECT_TARGET || "",
+    process.env.SPOTIFY_REDIRECT_URI || "",
     Scopes.userDetails
-  )
+  );
 
-  //   const items = await api.search("The Beatles", ["artist"]);
+  // If SDK exists display SpotifySearch otherwise empty
+  return sdk ? (<SpotifySearch sdk={sdk} />) : (<></>);
+}
 
-  //   console.table(items.artists.items.map((item) => ({
-  //       name: item.name,
-  //       followers: item.followers.total,
-  //       popularity: item.popularity,
-  //   })));
-  // })();
+export function useSpotify(clientId: string, redirectUrl: string, scopes: string[], config?: SdkOptions) {
+
+  const [sdk, setSdk] = useState<SpotifyApi | null>(null);
+  const { current: activeScopes } = useRef(scopes);
+
+  console.log("useSpotify", [clientId, redirectUrl, config, activeScopes]); // This line is logged
+
+  useEffect(() => {
+    console.log("useEffect", [clientId, redirectUrl, config, activeScopes]); // This line is not logged
+    (async () => {
+      const auth = new AuthorizationCodeWithPKCEStrategy(clientId, redirectUrl, activeScopes);
+      const internalSdk = new SpotifyApi(auth, config);
+
+      try {
+        const { authenticated } = await internalSdk.authenticate();
+
+        if (authenticated) {
+          setSdk(() => internalSdk);
+        }
+      } catch (e: Error | unknown) {
+
+        const error = e as Error;
+        if (error && error.message && error.message.includes("No verifier found in cache")) {
+          console.error("If you are seeing this error in a React Development Environment it's because React calls useEffect twice. Using the Spotify SDK performs a token exchange that is only valid once, so React re-rendering this component will result in a second, failed authentication. This will not impact your production applications (or anything running outside of Strict Mode - which is designed for debugging components).", error);
+        } else {
+          console.error(e);
+        }
+      }
+    })();
+  }, [clientId, redirectUrl, config, activeScopes]);
+
+  return sdk;
+}
+
+function SpotifySearch({ sdk }: { sdk: SpotifyApi}) {
+  const [results, setResults] = useState<SearchResults>({} as SearchResults);
+
+  useEffect(() => {
+    (async () => {
+      const results = await sdk.search("The Beatles", ["artist"]);
+      setResults(() => results);      
+    })();
+  }, [sdk]);
+
+  // Generate a table for the results
+  const tableRows = results.artists?.items.map((artist) => {
+    return (
+      <tr key={artist.id}>
+        <td>{artist.name}</td>
+        <td>{artist.popularity}</td>
+        <td>{artist.followers.total}</td>
+      </tr>
+    );
+  });
 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-      </div>
-    </main>
+    <>
+      <h1>Spotify Search for The Beatles</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Followers</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableRows}
+        </tbody>
+      </table>
+    </>
   )
 }
